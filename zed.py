cf4f1887d40032cf9f2026823f96305818d94447
pereos <danieljiang2358@gmail.com>
2023-02-09 16:37:01 -0500

    gonna fine tune soon

--- zed.py
+++ zed.py
@@ -66,65 +66,16 @@ def get_april_tag():
         depth = []
         tag_id = []
 
-        debug_image = copy.deepcopy(image_zed.get_data())
-
-        debug_image = draw_tags(debug_image, tags)
-
-        key = cv2.waitKey(1)
-        if key == 27:
-            cv2.destroyAllWindows()
-            sys.exit()
+        print(len(tags))
             
-        cv2.imshow('AprilTags', debug_image)
-
         # Finds the depth of each AprilTag in the image
-        if tags:
-            for tag in tags:
-                err, point_cloud_value = point_cloud.get_value(*tag.center)
+        for tag in tags:
+            err, point_cloud_value = point_cloud.get_value(*tag.center)
 
-                point_cloud_x.append(point_cloud_value[0])
-                point_cloud_y.append(point_cloud_value[1])
-                point_cloud_z.append(point_cloud_value[2])
-                depth.append(depth_map.get_value(*tag.center)[1])
-                tag_id.append(tag.tag_id)
-
-            print("-------")
-            print(tag.pose_err)
-
-            if tag.tag_id == 3:
-                print("YO MR. WHITE")
-                print(tag.pose_R)
-                print(tag.pose_t)
-                print(tag.tag_id)
+            point_cloud_x.append(point_cloud_value[0])
+            point_cloud_y.append(point_cloud_value[1])
+            point_cloud_z.append(point_cloud_value[2])
+            depth.append(depth_map.get_value(*tag.center)[1])
+            tag_id.append(tag.tag_id)
 
         return point_cloud_x, point_cloud_y, point_cloud_z, depth, tag_id
-
-
-def draw_tags(image, tags):
-    for tag in tags:
-        tag_family = tag.tag_family
-        tag_id = tag.tag_id
-        center = tag.center
-        corners = tag.corners
-
-        center = (int(center[0]), int(center[1]))
-        corner_01 = (int(corners[0][0]), int(corners[0][1]))
-        corner_02 = (int(corners[1][0]), int(corners[1][1]))
-        corner_03 = (int(corners[2][0]), int(corners[2][1]))
-        corner_04 = (int(corners[3][0]), int(corners[3][1]))
-
-        cv2.circle(image, (center[0], center[1]), 5, (0, 0, 255), 2)
-
-        cv2.line(image, (corner_01[0], corner_01[1]),
-                (corner_02[0], corner_02[1]), (255, 0, 0), 2)
-        cv2.line(image, (corner_02[0], corner_02[1]),
-                (corner_03[0], corner_03[1]), (255, 0, 0), 2)
-        cv2.line(image, (corner_03[0], corner_03[1]),
-                (corner_04[0], corner_04[1]), (0, 255, 0), 2)
-        cv2.line(image, (corner_04[0], corner_04[1]),
-                (corner_01[0], corner_01[1]), (0, 255, 0), 2)
-
-        cv2.putText(image, str(tag_id), (center[0] - 10, center[1] - 10),
-                   cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
-
-    return image
