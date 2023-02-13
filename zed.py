b0ff41aee19ab6a86ff65374ddcdb904d35fbaa3
pereos <danieljiang2358@gmail.com>
2023-02-13 15:08:35 -0500

    oof

--- zed.py
+++ zed.py
@@ -98,3 +98,31 @@ def get_april_tag():
 
         return point_cloud_x, point_cloud_y, point_cloud_z, depth, tag_id
 
+def draw_tags(image, tags):
+    for tag in tags:
+        tag_family = tag.tag_family
+        tag_id = tag.tag_id
+        center = tag.center
+        corners = tag.corners
+
+        center = (int(center[0]), int(center[1]))
+        corner_01 = (int(corners[0][0]), int(corners[0][1]))
+        corner_02 = (int(corners[1][0]), int(corners[1][1]))
+        corner_03 = (int(corners[2][0]), int(corners[2][1]))
+        corner_04 = (int(corners[3][0]), int(corners[3][1]))
+
+        cv2.circle(image, (center[0], center[1]), 5, (0, 0, 255), 2)
+
+        cv2.line(image, (corner_01[0], corner_01[1]),
+                (corner_02[0], corner_02[1]), (255, 0, 0), 2)
+        cv2.line(image, (corner_02[0], corner_02[1]),
+                (corner_03[0], corner_03[1]), (255, 0, 0), 2)
+        cv2.line(image, (corner_03[0], corner_03[1]),
+                (corner_04[0], corner_04[1]), (0, 255, 0), 2)
+        cv2.line(image, (corner_04[0], corner_04[1]),
+                (corner_01[0], corner_01[1]), (0, 255, 0), 2)
+
+        cv2.putText(image, str(tag_id), (center[0] - 10, center[1] - 10),
+                   cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+
+    return image
